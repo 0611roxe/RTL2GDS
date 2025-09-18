e5841479446f0521ee1a7c94a3b917e3fef8bf99
HanzhangLiu <1304006240@qq.com>
2025-09-18 17:29:29 +0800

    Update ysyx-workbench submodule pointer

--- tools/ysyx-workbench/am-kernels/tests/cpu-tests/tests/dummy.c
+++
@@ -1,7 +0,0 @@
-#define CLINT_ADDR 0x02000048L
-void a(void)  {
-}
-int main() {
-  a();
-  return 0;
-}
--- tools/ysyx-workbench/am-kernels/tests/klib-tests/tests/malloc_test.c
+++
@@ -1,7 +0,0 @@
-#include "klibtest.h"
-
-int main() {
-    char *p = (char *)malloc(5);
-    printf("%d\n",sizeof(p));
-    return 0;
-}
